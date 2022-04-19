501f1e9895a4a2265a6e36ba52584e6fb349c755
qyt <486179@qq.com>
2022-04-19 10:01:05 +0800

    support vscopy x86 and x64

---
+++ XEngine_Source/VSCopy-x64.bat
@@ -0,0 +1,11 @@
+copy /y "%XEngine_Lib64%\XEngine_BaseLib\XEngine_BaseLib.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_SystemSdk\XEngine_SystemApi.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_HelpComponents\HelpComponents_XLog.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_DownLoad\XEngine_DownLoad.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_NetHelp\NetHelp_APIHelp.dll" "./"
+
+copy /y "%XEngine_Lib64%\XEngine_HelpComponents\zlib1.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_LibEx\libcrypto-3.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_LibEx\libssl-3.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_LibEx\libcurl.dll" "./"
+copy /y "%XEngine_Lib64%\XEngine_LibEx\nghttp2.dll" "./"
