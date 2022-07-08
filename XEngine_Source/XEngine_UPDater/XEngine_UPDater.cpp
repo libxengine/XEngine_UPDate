641b566542f2ccf53ce22cb63b5b7c51e652b232
qyt <486179@qq.com>
2022-07-08 14:20:33 +0800

    modify:make read parament

--- XEngine_Source/XEngine_UPDater/XEngine_UPDater.cpp
+++ XEngine_Source/XEngine_UPDater/XEngine_UPDater.cpp
@@ -69,10 +69,16 @@ int main(int argc, char** argv)
 	signal(SIGTERM, Signale_Handler);
 	signal(SIGABRT, Signale_Handler);
 
-	if (st_ServiceConfig.bIsMake)
+	if (st_ServiceConfig.st_Maker.bIsMake)
 	{
 		XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("开始构建版本列表"));
-		if (!HelpModule_Api_BuildVer(st_ServiceConfig.st_Maker.tszMakePath, st_ServiceConfig.tszLocalList, st_ServiceConfig.st_Maker.tszUPFile, st_ServiceConfig.st_Maker.tszUPUrl))
+		TCHAR tszFilePath[MAX_PATH];
+		TCHAR tszFileName[MAX_PATH];
+
+		memset(tszFilePath, '\0', MAX_PATH);
+		memset(tszFileName, '\0', MAX_PATH);
+		BaseLib_OperatorString_GetFileAndPath(st_ServiceConfig.st_Maker.tszUPUrl, tszFilePath, tszFileName);
+		if (!HelpModule_Api_BuildVer(st_ServiceConfig.st_Maker.tszMakePath, st_ServiceConfig.tszLocalList, tszFileName, tszFilePath))
 		{
 			XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("构建标准版本列表失败！错误：%lX"), UPHelpModule_GetLastError());
 			goto NETSERVICE_APPEXIT;
